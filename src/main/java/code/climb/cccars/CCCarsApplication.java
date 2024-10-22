5a8f8f801e51414b2f34da852210991eea76cb10
Mohammed SHIHA <mohammedshiha91@gmail.com>
2024-10-22 11:45:59 +0300

    Initial Commit

---
+++ src/main/java/code/climb/cccars/CCCarsApplication.java
@@ -0,0 +1,11 @@
+package code.climb.cccars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CCCarsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CCCarsApplication.class, args);
+    }
+}
